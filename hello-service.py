b6021efd293ae5892dc1ff046682c5fc297d2283
Jan Vandlíček <jan.vandlicek@gmail.com>
2024-11-22 11:26:48 +0100

    Added hostname to greeting msg

--- hello-service.py
+++ hello-service.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import socket
 
 app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello(name):
-    message = f'Hello {name}! How are you?'
+    hostname = socket.gethostbyname()
+    message = f'Hello {name}! My name is {hostname}.'
     response = f'{{ message: "{message}"}}\r\n'
     code = 200
     return response, code, {'Content-Type': 'application/json'}
